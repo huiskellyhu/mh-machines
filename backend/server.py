e008ce92742e8102abbad00591121ce7479794f2
huiskellyhu <kellyhu96@gmail.com>
2023-09-15 00:50:38 -0700

    Created database file

--- backend/server.py
+++ backend/server.py
@@ -1,7 +1,8 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 CORS(app, supports_credentials=True)
 
 @app.route("/")
