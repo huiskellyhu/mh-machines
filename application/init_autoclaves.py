7ed1b19213d4f2636d0253fb4a5204765816485b
huiskellyhu <kellyhu96@gmail.com>
2023-09-24 20:42:57 -0700

    Updated database schema to include machine's "setting"

--- application/init_autoclaves.py
+++ application/init_autoclaves.py
@@ -7,14 +7,14 @@ with open('application\schema.sql') as f:
 
 cursor = connection.cursor()
 
-insert_query = "INSERT INTO autoclaves (id, mname, stat, created) VALUES (?, ?, ?, ?)"
-ac_to_insert = [(0,'WASHER','FREE','2023-09-18 00:00:00'),
-            (1,'AUTOCLAVE 1','FREE','2023-09-18 00:00:00'),
-            (2,'AUTOCLAVE 2','FREE','2023-09-18 00:00:00'),
-            (3,'AUTOCLAVE 3','FREE','2023-09-18 00:00:00'),
-            (4,'AUTOCLAVE 4','FREE','2023-09-18 00:00:00'),
-            (5,'AUTOCLAVE 5','FREE','2023-09-18 00:00:00'),
-            (6,'AUTOCLAVE 6','FREE','2023-09-18 00:00:00')]
+insert_query = "INSERT INTO autoclaves (id, mname, stat, created, setting) VALUES (?, ?, ?, ?, ?)"
+ac_to_insert = [(0,'WASHER','FREE','2023-09-18 00:00:00', "LIQUID15"),
+            (1,'AUTOCLAVE 1','FREE','2023-09-18 00:00:00',"LIQUID15"),
+            (2,'AUTOCLAVE 2','FREE','2023-09-18 00:00:00',"LIQUID15"),
+            (3,'AUTOCLAVE 3','FREE','2023-09-18 00:00:00',"LIQUID15"),
+            (4,'AUTOCLAVE 4','FREE','2023-09-18 00:00:00',"LIQUID15"),
+            (5,'AUTOCLAVE 5','FREE','2023-09-18 00:00:00',"LIQUID15"),
+            (6,'AUTOCLAVE 6','FREE','2023-09-18 00:00:00',"LIQUID15")]
 
 cursor.executemany(insert_query, ac_to_insert)
 
